f26dd94585073aaf8e817d8d60adcc3d1014c35b
Sibtain Raza <khanbhai14063@gmail.com>
2026-08-03 17:41:25 +0530

    Change For Deployment

--- backend/server.js
+++ backend/server.js
@@ -9,14 +9,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Routes (ADD THIS 👇)
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 app.use("/api/jobs", require("./routes/jobRoutes"));
 app.use("/api/applications", require("./routes/applicationRoutes"));
-app.use("/api/user", require("./routes/userRoutes"))
+app.use("/api/user", require("./routes/userRoutes"));
 
 const adminRoutes = require("./routes/adminRoutes");
-
 app.use("/api/admin", adminRoutes);
 
 // Test route
@@ -30,8 +29,10 @@ mongoose
   .then(() => {
     console.log("✅ MongoDB Connected Successfully");
 
-    app.listen(5000, () => {
-      console.log(" Server running on port 5000");
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`🚀 Server running on port ${PORT}`);
     });
   })
   .catch((err) => {
